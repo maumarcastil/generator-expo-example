10222ef308bfe55489d3eacc5c112313be0f24da
mauricio <maumarcastil@gmail.com>
2024-07-16 15:36:37 -0500

    chore: Update eslint configuration to extend additional plugins and rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,35 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: 'expo',
+  extends: [
+    "expo",
+    "plugin:import/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+  ],
+  plugins: ["prettier", "import", "simple-import-sort"],
+  rules: {
+    "prettier/prettier": "error",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          ["builtin", "external"],
+          ["internal"],
+          ["parent", "sibling", "index"],
+          ["unknown", "type"],
+        ],
+        "newlines-between": "always",
+        alphabetize: { order: "asc", caseInsensitive: true },
+      },
+    ],
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
 };
